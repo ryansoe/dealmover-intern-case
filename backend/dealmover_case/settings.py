626ee5a86c1df3a78b26bc044bac39fc166807ca
ryansoe <ryansoe90686@gmail.com>
2025-09-14 20:46:04 -0700

    fix: changed 'backend' to 'dealmover_case' in settings

--- backend/dealmover_case/settings.py
+++ backend/dealmover_case/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'dealmover_case.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'dealmover_case.wsgi.application'
 
 
 # Database
